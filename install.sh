f3e4746f8af4397bbf97d215264dff2280e762f0
QJesus <208780931@qq.com>
2019-12-03 22:57:16 +0800

    mystique linux 下的安装脚本

--- install.sh
+++ install.sh
@@ -1,13 +1,24 @@
-./dotnet publish -r linux-x64 -o /var/www/mystique
+#!/bin/bash
 
-cd /etc/systemd/system
+if [ ! -d "/opt/smt/eusb_terminal/" ]; then
+    mkdir -p /opt/smt/eusb_terminal/
+    echo "mkdir /opt/smt/eusb_terminal/"
+fi
 
-echo "[Unit]
+if [ ! -d "$(cd `dirname $0`; pwd)/Mystique" ]; then
+    echo "error. $(cd `dirname $0`; pwd)/Mystique not found"
+else
+    firewall-cmd --zone=public --remove-port=12580/tcp --permanent
+    systemctl daemon-reload
+    systemctl stop Mystique.service
+    systemctl disable Mystique.service
+
+    echo "[Unit]
 Description=Mystique
 
 [Service]
-WorkingDirectory=/var/www/mystique
-ExecStart=/var/www/mystique/Mystique --urls=http://*:5050
+WorkingDirectory=/opt/smt/eusb_terminal/Mystique
+ExecStart=/opt/smt/eusb_terminal/Mystique/Mystique --urls=http://*:12580
 Restart=always
 RestartSec=12
 KillSignal=SIGINT
@@ -19,13 +30,18 @@ Environment=ASPNETCORE_ENVIRONMENT=Production
 Environment=DOTNET_PRINT_TELEMETRY_MESSAGE=false
 
 [Install]
-WantedBy=multi-user.target" >> mystique.service
+WantedBy=multi-user.target" >/etc/systemd/system/Mystique.service
+ 
+    cp -r $(cd `dirname $0`; pwd)/Mystique /opt/smt/eusb_terminal/
+    echo "cp -r /opt/smt/eusb_terminal/Mystique"
 
-systemctl restart rsyslog
+    # systemctl restart rsyslog
+    systemctl enable Mystique.service
+    systemctl start Mystique.service
+fi
 
-systemctl enable mystique.service
-systemctl start  mystique.service
-
-firewall-cmd --zone=public --add-port=5050/tcp --permanent
+firewall-cmd --zone=public --add-port=12580/tcp --permanent
 firewall-cmd --reload
+echo "export host port 12580"
 
+systemctl status Mystique.service -l
